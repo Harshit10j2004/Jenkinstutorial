77062f31db5d192505e198e681469dfc7693dba9
harshit <onxy.harsh123@gmail.com>
2025-07-08 00:35:36 +0530

    the test file (api, response)

---
+++ tests/test_qr_maker.py
@@ -0,0 +1,17 @@
+import pytest
+
+from fastapi.testclient import TestClient
+from qr_maker import app
+
+client = TestClient(app)
+
+@pytest.mark.parametrize("url" , ["https://www.youtube.com/",
+                                  "https://www.tutorialspoint.com/python/index.htm",
+                                  "https://www.w3schools.com/"])
+
+def testing(url):
+
+    response = client.post("/qrgen", data={"data": url})
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "image/png"
+    assert len(response.content) > 0
