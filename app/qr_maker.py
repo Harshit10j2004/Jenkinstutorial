16454ea9de713c06cf73c1bae1b05562710d0aaf
Harshit gupta <onxy.harsh123@gmail.com>
2025-07-09 20:47:10 +0530

    Update qr_maker.py

--- app/qr_maker.py
+++ app/qr_maker.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI,Form
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 
-# Triggering Jenkins test build 🚀
 
 app = FastAPI()
 
