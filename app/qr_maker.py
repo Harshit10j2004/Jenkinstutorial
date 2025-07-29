a35040d346cdd9e293768a52421bd14be3869837
harshit <onxy.harsh123@gmail.com>
2025-07-29 15:06:58 +0530

    added docker compose file for local testing and all things are debuged local testing sucessfull

--- app/qr_maker.py
+++ app/qr_maker.py
@@ -3,15 +3,15 @@ import qrcode
 from fastapi import FastAPI,Form
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 
 app = FastAPI()
 
-origin = ["http://localhost:63342"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins= origin,
+    allow_origins= ["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -20,27 +20,33 @@ app.add_middleware(
 @app.post('/qrgen')
 
 def qrgene(data: str = Form(...)):
+    print("Received data:", data)
 
-    qr = qrcode.QRCode(
+    try:
 
-        version = 1 ,
-        error_correction = qrcode.constants.ERROR_CORRECT_L,
-        box_size = 10,
-        border = 4
+        qr = qrcode.QRCode(
 
-    )
+            version = 1 ,
+            error_correction = qrcode.constants.ERROR_CORRECT_L,
+            box_size = 10,
+            border = 4
 
-    qr.add_data(data)
-    qr.make(fit = True)
+        )
 
-    img = qr.make_image(fill_color = 'black' , back_color = 'white')
+        qr.add_data(data)
+        qr.make(fit = True)
 
-    buffer = BytesIO()
-    img.save(buffer, format="PNG")
-    buffer.seek(0)
+        img = qr.make_image(fill_color = 'black' , back_color = 'white')
 
-    return StreamingResponse(buffer, media_type="image/png")
+        buffer = BytesIO()
+        img.save(buffer, format="PNG")
+        buffer.seek(0)
 
-    
+        return StreamingResponse(buffer, media_type="image/png")
+
+    except Exception as e:
+        print(e)
+        return JSONResponse(status_code=500, content={"issue": str(e)})
+
 
 
